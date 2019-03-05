b430e34f7817b01638514d285c45d1ef8ccccf68
luhongdeng <luka877@outlook.com>
2019-03-05 14:07:37 +0800

    del 金陵中学副本

--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import { Button } from 'antd-mobile';
-import { Button as Buttonb } from 'antd';
 
 class Search extends Component {
   render() {
     return (
       <div>
         查询页面，用于公众号
-        <Button>antd-mobile</Button>
-        <Buttonb>antd</Buttonb>
       </div>
     );
   }
