b9929c6a7c08ac5f40c98c23186345850ac27ce8
Eden <877898552@qq.com>
2019-03-06 15:07:37 +0800

    图片上传

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -243,20 +243,8 @@ module.exports = function(webpackEnv) {
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
-        chunks: 'all',
-        minSize:30000,
-        maxAsyncRequests:5,
-        maxInitialRequests:3,
-        automaticNameDelimiter:'~',
-        name:true,
-        cacheGroups:{
-          vendors:{
-            chunks:'all',
-            test:/antd/,
-            priority:100,
-            name:'vendors',
-          }
-        }
+        chunks: 'initial',
+        name:false,
       },
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
