edeeac15c5349cfa70346816b759896757dc08d0
jbone123 <jb418426936@qq.com>
2019-03-12 10:34:42 +0800

    ceshi

--- src/component/Choose/Choose.js
+++ src/component/Choose/Choose.js
@@ -17,35 +17,35 @@ export default class Choose extends Component {
   }
   
   componentDidMount() {
-    let nowDate = moment().valueOf()
-    let startDate = moment('2019-06-1').valueOf()
-    let date = nowDate-startDate
+    // let nowDate = moment().valueOf()
+    // let startDate = moment('2019-06-1').valueOf()
+    // let date = nowDate-startDate
+    // if(date< 0){
+    //   this.setState({
+    //     clikeBut: 0
+    //   });
+    // }
+    resultApi().then(res => {
+      if (res.data.code == "200") {
+        console.log(res.data,'shuju')
+        const datainter = res.data.data.interviewResult;
+        this.setState({
+          interviewResult: datainter
+        });
+        if ((datainter && datainter) == null) {
+          this.setState({
+            clikeBut: 0
+          });
+        }
+      }
+    });
     
-    // resultApi().then(res => {
-    //   if (res.data.code == "200") {
-    //     console.log(res.data,'shuju')
-    //     const datainter = res.data.data.interviewResult;
-    //     this.setState({
-    //       interviewResult: datainter
-    //     });
-    //     if ((datainter && datainter) == null) {
-    //       this.setState({
-    //         clikeBut: 0
-    //       });
-    //     }
-    //   }
-    // });
-    if(date< 0){
-      this.setState({
-        clikeBut: 0
-      });
-    }
   }
   goGrade = () => {
-    // const typeFay = this.state.interviewResult && this.state.interviewResult;
-    if (this.state.clikeBut == 0) {
+    const typeFay = this.state.interviewResult && this.state.interviewResult;
+    // if (this.state.clikeBut == 0) {
+      if (typeFay == null) {
       message.info("未到查询时间，不能查询");
-      // alert("成绩暂未公布")
     } else {
       this.props.history.push({
         pathname: "/result"
@@ -67,7 +67,6 @@ export default class Choose extends Component {
     });
   };
   render() {
-    console.log(this.state.interviewResult);
     const  {clikeBut} =this.state
     return (
       <div className="detail">
